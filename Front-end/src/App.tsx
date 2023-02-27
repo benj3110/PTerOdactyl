a5b2432229167261ef02670227991ebc662978c8
Mahir Ahmed <79879216+Mahir-19@users.noreply.github.com>
2023-02-27 16:17:37 +0000

    pto dashboard

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import PTODashboard from "./PTODashboard";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <p> hi</p>
+      <PTODashboard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
