abace3d848df903d391ed3aa4ad7cbeb389a6cbc
benitovark <benito.varghese@raytheon.co.uk>
2023-03-01 10:24:08 +0000

    backend

--- Back-end/app.js
+++ Back-end/app.js
@@ -3,3 +3,11 @@ const cors = require("cors");
 const express = require("express");
 
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
