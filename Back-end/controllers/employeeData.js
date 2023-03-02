ecc05927616db4bb5c840d579678c625f249d8cf
benitovark <benito.varghese@raytheon.co.uk>
2023-03-02 14:41:08 +0000

    employee data get
    getting all employee data from mongo done

--- Back-end/controllers/employeeData.js
+++ Back-end/controllers/employeeData.js
@@ -1,8 +1,9 @@
 const Employee = require("../models/employee")
+const asyncHandler = require("express-async-handler")  //used instead of try catch blocks and handling errors, wrapping the async func 
 
-const getEmployeeData = (req, res, next) => {
-	const employeeData = Employee.find();
+const getEmployeeData = asyncHandler(async (req, res, next) => {
+	const employeeData = await Employee.find();
 	res.json(employeeData);
-};
+}) ;
 
 module.exports = { getEmployeeData };
--- Back-end/models/Employee.js
+++ Back-end/models/Employee.js
@@ -4,7 +4,11 @@ const EmployeeSchema = new mongoose.Schema({
 	EmployeeID: { type: String, required: true },
 	Name: { type: String, required: true },
 	Role: { type: String, required: true },
-	PTOAllowance: { type: String, required: true },
+	Allowance: { type: String, required: true },
+	Remaining: { type: String, required: true },
+	UsedDates: { type: String, required: false }, //todo turn into list and turn dates into date types?
+	CarriedOver: { type: String, required: false },
+	PendingDates: { type: String, required: false }, //todo turn into list
 });
 
 module.exports = mongoose.model("employees", EmployeeSchema);
