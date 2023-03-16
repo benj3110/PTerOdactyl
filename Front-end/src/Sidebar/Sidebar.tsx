34c627ea6abdfd246f6a27a04f0013b4005a85ea
Mahir Ahmed <79879216+Mahir-19@users.noreply.github.com>
2023-03-16 13:48:44 +0000

    improved ui/ux for sidebar for better accesssiibiloty

--- Front-end/src/Sidebar/Sidebar.tsx
+++ Front-end/src/Sidebar/Sidebar.tsx
@@ -4,40 +4,38 @@ import logo from "../Raytheon_Technologies_logo.png";
 import { Link } from "react-router-dom";
 
 interface sidebarProps {
-	isManager: boolean;
-	loggedIn: boolean;
+  isManager: boolean;
+  loggedIn: boolean;
 }
 
 const Sidebar: React.FC<sidebarProps> = ({ isManager, loggedIn }) => {
-	return (
-		<div className="sidebar">
-			<div className="logo">
-				<img src={logo} alt="My Company Logo" />
-			</div>
-			<nav>
-				<ul className="NavBar-Links">
-					<li>
-						<Link to="/bookingForm"> Book Time Off</Link>
-					</li>
-					<li>
-						<Link to="/">Dashboard</Link>
-					</li>
-					<li>
-						{isManager == true && (
-							<Link to="/managersPage"> Manager </Link>
-						)}
-					</li>
-					<li>
-						{loggedIn == true ? (
-							<Link to="/signIn"> Sign Out</Link>
-						) : (
-							<Link to="/signIn"> Sign In</Link>
-						)}
-					</li>
-				</ul>
-			</nav>
-		</div>
-	);
+  return (
+    <div className="sidebar">
+      <div className="logo">
+        <img src={logo} alt="My Company Logo" />
+      </div>
+      <nav>
+        <ul className="NavBar-Links">
+          <li>
+            <Link to="/bookingForm">Book Time Off</Link>
+          </li>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            {isManager == true && <Link to="/managersPage">Manager</Link>}
+          </li>
+          <li>
+            {loggedIn == true ? (
+              <Link to="/signIn">Sign Out</Link>
+            ) : (
+              <Link to="/signIn">Sign In</Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Sidebar;
