941db470698d93b9c97ce593af8cd0908fb0dc7c
Mahir Ahmed <79879216+Mahir-19@users.noreply.github.com>
2023-03-21 16:53:14 +0000

    improvements to ui

--- Front-end/src/BookingPage/BookingForm.tsx
+++ Front-end/src/BookingPage/BookingForm.tsx
@@ -6,136 +6,134 @@ import { bookPTO } from "../utils";
 import "react-datepicker/dist/react-datepicker.css";
 import "./BookingForm.css";
 interface bookingProps {
-	name: string;
+  name: string;
 }
 
 const BookingForm: React.FC<bookingProps> = ({ name }) => {
-	const navigate: NavigateFunction = useNavigate();
+  const navigate: NavigateFunction = useNavigate();
 
-	const todaysDate: Date = new Date();
+  const todaysDate: Date = new Date();
 
-	//console.log(todaysDate.toISOString().substring(0, 16));
+  //console.log(todaysDate.toISOString().substring(0, 16));
 
-	const mins: number = todaysDate.getMinutes();
+  const mins: number = todaysDate.getMinutes();
 
-	if (mins < 29) {
-		todaysDate.setMinutes(29);
-	} else {
-		todaysDate.setMinutes(59);
+  if (mins < 29) {
+    todaysDate.setMinutes(29);
+  } else {
+    todaysDate.setMinutes(59);
 
-		//todaysDate.setHours(todaysDate.getHours() + 1);
-		//todaysDate.setMilliseconds(0);
-	}
+    //todaysDate.setHours(todaysDate.getHours() + 1);
+    //todaysDate.setMilliseconds(0);
+  }
 
-	//console.log(todaysDate);
+  //console.log(todaysDate);
 
-	const [startDate, setStartDate] = useState<Date>(todaysDate);
+  const [startDate, setStartDate] = useState<Date>(todaysDate);
 
-	const [endDate, setEndDate] = useState<Date | undefined>();
+  const [endDate, setEndDate] = useState<Date | undefined>();
 
-	console.log(name);
+  console.log(name);
 
-	const handleSubmit: () => void = async () => {
-		const bookingSubmit: {
-			name: string;
-			PendingDates: string;
-			startDate: string;
-			endDate: string | undefined;
-		} = {
-			name: name,
-			PendingDates: `${startDate
-				?.toISOString()
-				.substring(0, 16)} # ${endDate
-				?.toISOString()
-				.substring(0, 16)}`,
-			startDate: startDate?.toISOString().substring(0, 16),
-			endDate: endDate?.toISOString().substring(0, 16),
-		};
+  const handleSubmit: () => void = async () => {
+    const bookingSubmit: {
+      name: string;
+      PendingDates: string;
+      startDate: string;
+      endDate: string | undefined;
+    } = {
+      name: name,
+      PendingDates: `${startDate?.toISOString().substring(0, 16)} # ${endDate
+        ?.toISOString()
+        .substring(0, 16)}`,
+      startDate: startDate?.toISOString().substring(0, 16),
+      endDate: endDate?.toISOString().substring(0, 16),
+    };
 
-		await bookPTO(bookingSubmit);
-		navigate("/");
-	};
+    await bookPTO(bookingSubmit);
+    navigate("/");
+  };
 
-	const filterPassedTime: (time: Date) => boolean = (time: Date) => {
-		const selectedDate: Date = new Date(time);
-		const startTime: Date = new Date(time);
-		const endTime: Date = new Date(time);
-		startTime.setHours(9);
-		if (endTime.getDay() === 5) {
-			endTime.setHours(14);
-		} else {
-			endTime.setHours(17);
-		}
+  const filterPassedTime: (time: Date) => boolean = (time: Date) => {
+    const selectedDate: Date = new Date(time);
+    const startTime: Date = new Date(time);
+    const endTime: Date = new Date(time);
+    startTime.setHours(9);
+    if (endTime.getDay() === 5) {
+      endTime.setHours(14);
+    } else {
+      endTime.setHours(17);
+    }
 
-		if (
-			startTime.getTime() <= selectedDate.getTime() &&
-			selectedDate.getTime() < endTime.getTime()
-		) {
-			if (
-				todaysDate.toISOString().substring(0, 10) ===
-				selectedDate.toISOString().substring(0, 10)
-			) {
-				if (todaysDate.getTime() < selectedDate.getTime()) {
-					return true;
-				}
-			} else {
-				return true;
-			}
-		}
-		return false;
-	};
+    if (
+      startTime.getTime() <= selectedDate.getTime() &&
+      selectedDate.getTime() < endTime.getTime()
+    ) {
+      if (
+        todaysDate.toISOString().substring(0, 10) ===
+        selectedDate.toISOString().substring(0, 10)
+      ) {
+        if (todaysDate.getTime() < selectedDate.getTime()) {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    }
+    return false;
+  };
 
-	return (
-		<div className="box">
-			<div className="hero">
-				<video autoPlay loop muted id="video"></video>
-			</div>
-			<div className="Second-comp">
-				<div className="BookingForm">
-					<div className="booking-formTitle">
-						<h1>Book PTO</h1>
-					</div>
+  return (
+    <div className="box">
+      <div className="hero">
+        <video autoPlay loop muted id="video"></video>
+      </div>
+      <div className="Second-comp">
+        <div className="BookingForm">
+          <div className="booking-formTitle">
+            <h1>Book PTO</h1>
+          </div>
 
-					<div>
-						From:
-						<DatePicker
-							selected={startDate}
-							onChange={(date: Date) => setStartDate(date)}
-							showTimeSelect
-							minDate={todaysDate}
-							filterTime={filterPassedTime}
-							//minTime={setHours(setMinutes(new Date(), 0), 17)}
-							//maxTime={setHours(setMinutes(new Date(), 30), 20)}
-							dateFormat="do MMMM Y HH:mm"
-							fixedHeight
-						/>
-					</div>
-					<div>
-						To:
-						<DatePicker
-							selected={endDate}
-							onChange={(date: Date) => setEndDate(date)}
-							showTimeSelect
-							minDate={startDate ? startDate : todaysDate}
-							//minTime={setHours(setMinutes(new Date(), 0), 17)}
-							//maxTime={setHours(setMinutes(new Date(), 30), 20)}
-							dateFormat="do MMMM Y HH:mm"
-							fixedHeight
-						/>
-					</div>
-					<button
-						className="bookPTOButton"
-						onClick={() => {
-							handleSubmit();
-						}}
-					>
-						{" "}
-						Submit{" "}
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+          <div className="booking-formTitle">
+            From:
+            <DatePicker
+              selected={startDate}
+              onChange={(date: Date) => setStartDate(date)}
+              showTimeSelect
+              minDate={todaysDate}
+              filterTime={filterPassedTime}
+              //minTime={setHours(setMinutes(new Date(), 0), 17)}
+              //maxTime={setHours(setMinutes(new Date(), 30), 20)}
+              dateFormat="do MMMM Y HH:mm"
+              fixedHeight
+            />
+          </div>
+          <div className="booking-formTitle">
+            To:
+            <DatePicker
+              selected={endDate}
+              onChange={(date: Date) => setEndDate(date)}
+              showTimeSelect
+              minDate={startDate ? startDate : todaysDate}
+              //minTime={setHours(setMinutes(new Date(), 0), 17)}
+              //maxTime={setHours(setMinutes(new Date(), 30), 20)}
+              dateFormat="do MMMM Y HH:mm"
+              fixedHeight
+            />
+          </div>
+          <button
+            className="bookPTOButton"
+            onClick={() => {
+              handleSubmit();
+            }}
+          >
+            {" "}
+            Submit{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BookingForm;
--- Front-end/src/Sign in/SignIn.tsx
+++ Front-end/src/Sign in/SignIn.tsx
@@ -2,80 +2,80 @@ import React, { useState, useEffect } from "react";
 import { getEmployeeData } from "../utils";
 import { useNavigate, NavigateFunction } from "react-router-dom";
 interface loginInterface {
-	name: string;
-	setName: React.Dispatch<React.SetStateAction<string>>;
-	loggedIn: boolean;
-	setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
-	isManager: boolean;
-	setIsManager: React.Dispatch<React.SetStateAction<boolean>>;
+  name: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
+  loggedIn: boolean;
+  setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  isManager: boolean;
+  setIsManager: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const LoginPage: React.FC<loginInterface> = ({
-	name,
-	setName,
-	loggedIn,
-	setLoggedIn,
-	isManager,
-	setIsManager,
+  name,
+  setName,
+  loggedIn,
+  setLoggedIn,
+  isManager,
+  setIsManager,
 }) => {
-	const navigate: NavigateFunction = useNavigate();
+  const navigate: NavigateFunction = useNavigate();
 
-	const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setName(event.target.value);
-	};
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
 
-	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		try {
-			const empCheck = await getEmployeeData(name);
-			if (empCheck) {
-				empCheck.Role == "Manager" &&
-					(setIsManager(true), navigate("/managersPage"));
-				setLoggedIn(true);
-				empCheck.Role == "Employee" && navigate("/");
-			} else {
-				console.log("name doesn't match");
-			}
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const empCheck = await getEmployeeData(name);
+      if (empCheck) {
+        empCheck.Role == "Manager" &&
+          (setIsManager(true), navigate("/managersPage"));
+        setLoggedIn(true);
+        empCheck.Role == "Employee" && navigate("/");
+      } else {
+        console.log("name doesn't match");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	const handleClick: (event: any) => void = (event: any) => {
-		setLoggedIn(false);
-		setIsManager(false);
-		setName("");
-	};
+  const handleClick: (event: any) => void = (event: any) => {
+    setLoggedIn(false);
+    setIsManager(false);
+    setName("");
+  };
 
-	return (
-		<div>
-			{loggedIn == false ? (
-				<div>
-					<h1>Login</h1>
-					<form onSubmit={handleSubmit}>
-						<div>
-							<label htmlFor="name">Name</label>
-							<input
-								type="text"
-								id="name"
-								name="name"
-								value={name}
-								onChange={handleNameChange}
-							/>
-						</div>
-						<button type="submit">Submit</button>
-					</form>
-				</div>
-			) : (
-				<div>
-					<h1>Sign out</h1>
-					<button type="button" onClick={handleClick}>
-						Sign out
-					</button>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {loggedIn == false ? (
+        <div className="booking-formTitle">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={handleNameChange}
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <h1>Sign out</h1>
+          <button type="button" onClick={handleClick}>
+            Sign out
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default LoginPage;
