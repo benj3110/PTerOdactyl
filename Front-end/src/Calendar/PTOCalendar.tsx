fb9b68dacfa1bc3efbd8310bf8dc788705427136
Mahir Ahmed <79879216+Mahir-19@users.noreply.github.com>
2023-03-29 14:11:34 +0100

    added a calendar display

---
+++ Front-end/src/Calendar/PTOCalendar.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { EventInput } from "@fullcalendar/core";
+
+interface PTOData {
+  startDate: string;
+  endDate: string;
+  reason: string;
+}
+
+const PTOCalendar: React.FC = () => {
+  const [ptoData, setPtoData] = useState<PTOData[]>([]);
+
+  useEffect(() => {
+    // Fetch PTO data from backend API and update state
+    fetch("/api/pto")
+      .then((response) => response.json())
+      .then((data) => setPtoData(data));
+  }, []);
+
+  // Map PTO data to event format expected by FullCalendar
+  const events: EventInput[] = ptoData.map((pto) => ({
+    title: pto.reason,
+    start: pto.startDate,
+    end: pto.endDate,
+  }));
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+    />
+  );
+};
+
+export default PTOCalendar;
