f72b1b6cd3566101db87a5b725ea86dbbf4f606a
Mahir Ahmed <79879216+Mahir-19@users.noreply.github.com>
2023-04-05 09:24:57 +0100

    updatw

--- Front-end/src/Calendar/PTOCalendar.tsx
+++ Front-end/src/Calendar/PTOCalendar.tsx
@@ -13,27 +13,27 @@ const PTOCalendar: React.FC = () => {
   const [ptoData, setPtoData] = useState<PTOData[]>([]);
 
   useEffect(() => {
-    // Fetch PTO data from backend API and update state
     fetch("/api/pto")
       .then((response) => response.json())
-      .then((data) => setPtoData(data));
+      .then((data) => {
+        console.log(data);
+        setPtoData(data);
+      });
   }, []);
 
   // Map PTO data to event format expected by FullCalendar
-  // const events: EventInput[] = ptoData.map((pto) => ({
-  //   title: pto.reason,
-  //   start: pto.startDate,
-  //   end: pto.endDate,
-  // }));
+  const events: EventInput[] = ptoData.map((pto) => ({
+    title: pto.reason,
+    start: pto.startDate,
+    end: pto.endDate,
+    classNames: ["highlight"],
+  }));
 
   return (
     <FullCalendar
       plugins={[dayGridPlugin]}
       initialView="dayGridMonth"
-      events={[
-        { title: "event 1", date: "2023-31-03" },
-        { title: "event 2", date: "2023-31-03" },
-      ]}
+      events={events}
     />
   );
 };
